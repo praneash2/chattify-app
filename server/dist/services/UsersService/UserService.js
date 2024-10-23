c5fd840541cbf9e258621d4cf1857bff45e0c134
praneash2 <praneash2@gmail.com>
2024-10-24 00:34:34 +0530

    added jwt generation to the bakcend

--- server/dist/services/UsersService/UserService.js
+++ server/dist/services/UsersService/UserService.js
@@ -15,18 +15,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.UsersService = void 0;
 const UsersRepository_1 = require("../../repositories/UsersRepository");
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config({ path: ".env" });
 class UsersService {
     constructor() {
         this.getUser = (userId) => __awaiter(this, void 0, void 0, function* () {
             try {
                 const data = yield this.usersRepository.getUser(userId);
-                jsonwebtoken_1.default.sign({
-                    data: userId
-                }, 'secret', { expiresIn: '1h' });
-                return data;
+                const jwtToken = this.generateJwtToken(userId);
+                return Object.assign(Object.assign({}, data), { jwtToken: jwtToken });
             }
             catch (error) {
-                throw (error);
+                throw error;
             }
         });
         this.createUser = (user) => __awaiter(this, void 0, void 0, function* () {
@@ -36,12 +36,26 @@ class UsersService {
                     return { result: "Already Exists" };
                 }
                 const data = yield this.usersRepository.createUser(user);
-                return Object.assign(Object.assign({}, data), { result: "user created" });
+                const jwtToken = this.generateJwtToken(data.id);
+                return Object.assign(Object.assign({}, data), { result: "user created", jwtToken: jwtToken });
             }
             catch (error) {
-                throw (error);
+                throw error;
             }
         });
+        this.generateJwtToken = (userId) => {
+            const jwtSecret = process.env.JWT_SECRET;
+            if (jwtSecret) {
+                const signedData = jsonwebtoken_1.default.sign({
+                    data: userId,
+                }, jwtSecret, { expiresIn: "15d" });
+                return signedData;
+            }
+            else {
+                throw "internal error";
+            }
+            // return "";
+        };
         this.usersRepository = new UsersRepository_1.UsersRepository();
     }
 }
--- server/src/services/UsersService/UserService.ts
+++ server/src/services/UsersService/UserService.ts
@@ -1,56 +1,72 @@
 import { UsersRepository } from "../../repositories/UsersRepository";
-import jwt from 'jsonwebtoken';
-interface User{
-    name:string,
-    email:string,
-    password:string
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+interface User {
+  name: string;
+  email: string;
+  password: string;
 }
 
 interface userResult {
-    id?: number;
-    name?: string;
-    email?: string;
-    password?: string;
+  id?: number;
+  name?: string;
+  email?: string;
+  password?: string;
+  jwtToken?: string;
 }
 
-
-interface result{
-    result:string
+interface result {
+  result: string;
 }
 
-export class UsersService{
-    
-    private usersRepository:UsersRepository;
-    constructor(){
-        this.usersRepository = new UsersRepository();
+export class UsersService {
+  private usersRepository: UsersRepository;
+  constructor() {
+    this.usersRepository = new UsersRepository();
+  }
+
+  getUser = async (userId: number): Promise<userResult | null> => {
+    try {
+      const data = await this.usersRepository.getUser(userId);
+      const jwtToken:string=this.generateJwtToken(userId);
+      return {...data,jwtToken:jwtToken};
+    } catch (error) {
+      throw error;
     }
+  };
 
-    getUser=async(userId:number):Promise<userResult|null>=>{
-        try {
-            const data= await this.usersRepository.getUser(userId);
-            jwt.sign({
-                data: userId
-              }, 'secret', { expiresIn: '1h' });
-            return data;
-        } catch (error) {
-            throw(error)
-        }
-    
+  createUser = async (user: User): Promise<userResult & result> => {
+    try {
+      let userResultByEmail = await this.usersRepository.getUserByEmail(
+        user.email
+      );
+      if (userResultByEmail) {
+        return { result: "Already Exists" };
+      }
+      const data = await this.usersRepository.createUser(user);
+      const jwtToken:string=this.generateJwtToken(data.id);
+      return { ...data, result: "user created" , jwtToken:jwtToken};
+    } catch (error) {
+      throw error;
     }
+  };
 
-    createUser=async(user:User):Promise<userResult&result>=>{
-        try {
-            let userResultByEmail=await this.usersRepository.getUserByEmail(user.email);
-            if(userResultByEmail){
-                return {result:"Already Exists"}
-            }
-            const data=await this.usersRepository.createUser(user);
-            return {...data,result:"user created"};
-        } catch (error) {
-            throw(error);
-        }
-        
+  generateJwtToken = (userId: number): string => {
+    const jwtSecret = process.env.JWT_SECRET;
+
+    if (jwtSecret) {
+      const signedData = jwt.sign(
+        {
+          data: userId,
+        },
+        jwtSecret,
+        { expiresIn: "15d" }
+      );
+      return signedData;
+    } else {
+      throw "internal error";
     }
-
-
+    // return "";
+  };
 }
